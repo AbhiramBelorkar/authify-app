070dc7ff8e73f90f4b630077567e5c2e9d0671b7
abhirambelorkar <abhi25belorkar@gmail.com>
2025-07-21 21:55:43 +0530

    jwt dependency added

--- auth-backend/src/main/java/com/authify/app/util/JwtUtil.java
+++ auth-backend/src/main/java/com/authify/app/util/JwtUtil.java
@@ -1,8 +1,25 @@
 package com.authify.app.util;
 
 import org.hibernate.annotations.Comment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtUtil {
+
+    @Value("${jwt.secret.key}")
+    private String SECRET_KEY;
+
+    public String generateToken(UserDetails userDetails){
+        Map<String, Object> map = new HashMap<>();
+        return createToken(map, userDetails.getUsername());
+    }
+
+    private String createToken(Map<String, Object> map, String username) {
+
+    }
 }
